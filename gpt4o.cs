08eb9586e91a7c70b2cf2bd362e85905a4d03580
masroorposh10 <masroor.posh@symend.com>
2024-06-11 13:08:49 -0600

    Prompt fix

---
+++ gpt4o.cs
@@ -0,0 +1,16 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Initialize an array of integers
+        int[] numbers = new int[] { 1, 2, 3, 4, 5 };
+
+        // Print the array elements
+        foreach (int number in numbers)
+        {
+            Console.WriteLine(number);
+        }
+    }
+}
